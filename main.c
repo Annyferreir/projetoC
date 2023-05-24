3e6e814f11ca799e837fe8fc83714ec5843fa29e
anny <anny.ferreira2@etec.sp.gov.br>
2023-05-24 18:10:03 -0300

    novos arquivos

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+main() {
+
+int numero1;
+int numero2;
+
+numero1 = 10;
+numero2 = 20;
+
+printf("digite um valor para o numero 1:");
+scanf("%d" ,&numero1);
+printf("O numero 1: %d\nE o numero 2: %d" , numero1,numero2);	
+}
